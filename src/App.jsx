b8506cd661c7c1ee283bf64f96a0b0ca44aef279
Hrishabh-Inventurs <rishabh@inventurs.in>
2023-11-30 14:34:18 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// import { Box } from "@mui/material"
+import Header from "./components/Header/Header"
+import './style/props.css'
+
+const App = () => {
+
+  return (
+    <>
+      <Header />
+      {/* <Box
+        component="img"
+        sx={{ height: 400 }}
+        alt="Logo"
+        src="https://img-c.udemycdn.com/notices/web_carousel_slide/image/1c1f8809-69ec-43d7-8770-a145a714df9e.jpg"
+      /> */}
+    </>
+  )
+}
+
+export default App
