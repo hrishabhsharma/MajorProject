7e3a2b465ef903124dc7b299427d485f9b45be0e
Hrishabh-Inventurs <rishabh@inventurs.in>
2023-12-04 23:22:27 +0530

    update small changes

--- src/Compo/Register/Login.jsx
+++ src/Compo/Register/Login.jsx
@@ -32,6 +32,7 @@ function Login() {
                         localStorage.setItem("token", res.data.token);
                         console.log(res.data.userdetail)
                         navigate("/");
+                        window.location.reload(true)
                     }
                 })
                 .catch(err => console.log(err));
